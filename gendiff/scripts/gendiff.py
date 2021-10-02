68c6b9d0106fdded1b2b96627ff8d90a0eda6e57
airmage <airmage2@gmail.com>
2021-10-02 03:07:48 +0300

    First commit

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+
+def main():
+    print('Welcome to the Brain Games!')
+
+
+if __name__ == '__main__':
+    main()
