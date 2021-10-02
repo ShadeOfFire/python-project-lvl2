aac33ac23b3ef5e362291e4c406af86c77bceec5
airmage <airmage2@gmail.com>
2021-10-02 03:34:56 +0300

    Added parser

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,19 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("first_file", help="echo the string you use here")
+parser.add_argument("second_file", help="echo the string you use here")
+parser.add_argument("-f", "--format", help="set format of output")
 parser.parse_args()
+
+if args.f:
+    print("verbosity turned on")
+
+if args.format:
+    print("format")
+
+
+
 
 
 def main():
